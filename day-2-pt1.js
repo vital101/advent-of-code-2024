f40b014af13f6a5e64e5545bc952d70599d4ec32
Jack Slingerland <jack.slingerland@spoton.com>
2024-12-02 12:12:23 -0500

    Day 2 part 1

--- day-2-pt1.js
+++ day-2-pt1.js
@@ -1,6 +1,5 @@
 const { readFile } = require("fs/promises");
 
-
 (async () => {
     const rawData = await readFile("day-2.input.txt", "utf-8");
     const levels = rawData
@@ -8,31 +7,31 @@ const { readFile } = require("fs/promises");
         .map(row => {
             return row.split(" ").map(l => parseInt(l, 10));
         });
+    const safeLevels = levels.reduce((acc, level) => {
+        if (allInRange(level)) {
+            return acc + 1;
+        }
+        return acc;
+    }, 0);
+    console.log({ safeLevels });
 })();
 
-function isSafe(levels) {
-    
-}
-
-
-// WIP
-function allDecreasing(levels) {
+function allInRange(level) {
     let safe = true;
-    for (const i = 0; i < levels.length; i++) {
-        if (i === levels.length - 1) {
-            // no-op
-        } else if (levels[i] > levels[i+1]) {
+    for (let i = 0; i < level.length - 1; i++) {
+        const value = Math.abs(level[i] - level[i+1]);
+        if (value > 0 && value <= 3) {
             safe = true;
         } else {
-            safe = false;
+            return false;
         }
     }
-    return safe;
+    return verifyLevelIncreasingOrDecreasing(level);
 }
 
-function allDecreasing(levels) {
+function verifyLevelIncreasingOrDecreasing(level) {
+    const isDescending = level.every((num, i) => i === 0 || num < level[i-1]);
+    const isAscending = level.every((num, i) => i === 0 || num > level[i-1]);
+    return isAscending || isDescending;
 }
 
-function adjacentLevelsInRange(level) {
-
-}
